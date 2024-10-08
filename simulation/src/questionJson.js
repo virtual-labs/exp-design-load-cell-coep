0ccdf45147efe1391a3250a01933bc5917cb3fd1
vleadadmin <admin@vlabs.ac.in>
2024-10-08 12:05:28 +0000

    https://virtual-labs.github.io/exp-design-load-cell-coep click on the link to test your code.

---
+++ simulation/src/questionJson.js
@@ -0,0 +1,143 @@
+
+
+QuestionsJSON = {
+		"data": {
+			"SEC": [{
+
+				"QUES": [{
+					
+					"QC": "Electrical strain gauge works on the principle of __________",
+					"ANS": [{
+						"ANSID": true,
+						"content": "variation of resistance"
+					}, {
+						"ANSID": false,
+						"content": "variation of capacitance"
+					}, {
+						"ANSID": false,
+						"content": "variation of inductance"
+					},{
+						"ANSID": false,
+						"content": "variation of area "
+					}]
+				}]
+			}, {
+
+				"QUES": [{
+					
+					"QC": "Bonding element in a strain gauge must have __________",
+					"ANS": [{
+						"ANSID": false,
+						"content": "zero insulation resistance"
+					}, {
+						"ANSID": false,
+						"content": "low insulation resistance "
+					}, {
+						"ANSID": true,
+						"content": "high insulation resistance"
+					}, {
+						"ANSID": false,
+						"content": "infinite insulation resistance"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Commonly used elements for wire strain gauges are __________",
+					"ANS": [{
+						"ANSID": true,
+						"content": "nickel and copper"
+					}, {
+						"ANSID": false,
+						"content": "nickel and gold"
+					}, {
+						"ANSID": false,
+						"content": "gold and brass"
+					}, {
+						"ANSID": false,
+						"content": "silver and aluminium"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Proper functioning of a strain gauge depends on __________",
+					"ANS": [{
+						"ANSID": false,
+						"content": "strain"
+					}, {
+						"ANSID": false,
+						"content": "stress"
+					}, {
+						"ANSID": true,
+						"content": "bonding"
+					}, {
+						"ANSID": false,
+						"content": "length of wire"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Strain gauge measurement involves __________",
+					"ANS": [{
+						"ANSID": true,
+						"content": "Wheatstone bridge"
+					}, {
+						"ANSID": false,
+						"content": "Kelvin bridge"
+					}, {
+						"ANSID": false,
+						"content": "De Sauty's bridge"
+					}, {
+						"ANSID": false,
+						"content": "Anderson bridge"
+					}]
+					
+				}]
+			},{
+
+				"QUES": [{
+					
+					"QC": "Semiconductor strain gauge uses _______",
+					"ANS": [{
+						"ANSID": false,
+						"content": "rectifier circuitry"
+					}, {
+						"ANSID": false,
+						"content": "power electronics circuitry"
+					}, {
+						"ANSID": false,
+						"content": "ordinary bridge circuit"
+					}, {
+						"ANSID": true,
+						"content": "bridge circuit with temperature compensation"
+					}]
+					
+				}]
+			}
+			]
+		}
+
+
+	}
+
+
+
+var ANSWERJSON = {
+			
+				"0" : "All the points mentioned",
+				"1" : "Set the standard output for the required limits of process parameter",
+				"2" : "Set majority of the points of the transmitter output in the operating region",
+				"3" : "Pump discharge flow and Outlet flow",
+				"4" : "Nothing is required to be changed",
+				"5" : "Nil"
+				
+			
+}
